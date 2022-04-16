f48d1da59911572fa6b11d207a6515f8e2df8fe4
Fabienne-Guerin <fabienne.guerin]hotmail.fr>
2022-04-16 20:29:09 +0200

    updated to fix failures

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import './App.css';
 import QuoteCard from './QuoteCard';
 import axios from 'axios';
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 
 function App() {
   const [quote, setQuote] = useState({});
 
-  const getQuote =() => {
-  
+  const simpsonQuote = () => {
     axios
+   .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+   .then((response) => setQuote(response.data[0]))
+  }
+
+  useEffect(() => {
+       axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes") 
+      .then((response) => setQuote(response.data[0]))
   
-      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
-        
-      .then((response) => response.data)
+  },[]);
+      
   
-      .then((data) => {
-             
-        setQuote(data[0]);
-      });
-  
-  };
+
   return (
     <div className="App">
       <QuoteCard {...quote}/>
-      <button type="button" onClick={getQuote}>Get quote</button>
+      <button type="button" onClick={simpsonQuote}>Get quote</button>
     </div>
   );
 }
